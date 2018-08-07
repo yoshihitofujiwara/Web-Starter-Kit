48047802314d70fdf207e941731c9520c8453a23
yoshihitofujiwara <fujiwara.ink@gmail.com>
2018-08-07 18:22:55 +0900

    -pro圧縮追加

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
-const webpack = require("webpack");
+// const webpack = require("webpack");
+const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
 
 // リリースモード判定フラグ
 const IS_PRODUCTION = !!(process.argv[2] && process.argv[2].indexOf("-pro") != -1);
@@ -53,51 +54,66 @@ module.exports = {
 	watch: true,
 	mode: MODE,
 
-  entry: {
-    scripts: `${JS}/scripts/index.js`
-  },
+	entry: {
+		scripts: `${JS}/scripts/index.js`
+	},
 
-  output: {
-    filename: "[name].js"
-  },
+	output: {
+		filename: "[name].js"
+	},
 
-  module: {
-    rules: [
-      {
-        test: /\.js[x]?$/,
-        use: [{
-          loader: "babel-loader",
-          options: {
-            presets: [["env", {
+	module: {
+		rules: [
+			{
+				test: /\.js[x]?$/,
+				use: [{
+					loader: "babel-loader",
+					options: {
+						presets: [["env", {
 							targets: { browsers: ["last 2 versions"] },
 							modules: false
-            }]]
-          }
-        }],
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.(glsl|frag|vert)$/,
-        loader: "glslify-import-loader",
-        exclude: /node_modules/
-      },
-      {
-        test: /\.(glsl|frag|vert)$/,
-        loader: "raw-loader",
-        exclude: /node_modules/
-      },
-      {
-        test: /\.(glsl|frag|vert)$/,
-        loader: "glslify-loader",
-        exclude: /node_modules/
-      },
-      {
-        test: /\.(glsl|frag|vert)$/,
-        loader: "glsl-strip-comments",
-        exclude: /node_modules/
-      }
-    ]
-  },
+						}]]
+					}
+				}],
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.(glsl|frag|vert)$/,
+				loader: "glslify-import-loader",
+				exclude: /node_modules/
+			},
+			{
+				test: /\.(glsl|frag|vert)$/,
+				loader: "raw-loader",
+				exclude: /node_modules/
+			},
+			{
+				test: /\.(glsl|frag|vert)$/,
+				loader: "glslify-loader",
+				exclude: /node_modules/
+			},
+			{
+				test: /\.(glsl|frag|vert)$/,
+				loader: "glsl-strip-comments",
+				exclude: /node_modules/
+			}
+		]
+	},
 
-  plugins: plugins
+	plugins: plugins,
+
+	optimization: {
+		minimizer: IS_PRODUCTION ? [
+			new UglifyJSPlugin({
+				uglifyOptions: {
+					output: {
+						comments: /^\**!|@preserve|@license|@cc_on/
+					},
+					compress: {
+						drop_console: true
+					}
+				}
+			})
+		] : []
+	}
 };
